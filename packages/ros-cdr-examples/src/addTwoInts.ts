078142a1a0261d1e530c7f633027e94162b920fd
Rin Iwai <me@eyrin.jp>
2026-02-17 14:51:06 +0900

    fix: example

--- packages/ros-cdr-examples/src/addTwoInts.ts
+++ packages/ros-cdr-examples/src/addTwoInts.ts
@@ -3,13 +3,14 @@ import * as ros from '@eyr1n/ros-cdr-serialization';
 
 const url = process.argv[2] ?? 'ws://127.0.0.1:54321';
 
-const AddTwoIntsRequest = ros.message('example_interfaces/srv/AddTwoInts', {
-  a: ros.int64(),
-  b: ros.int64(),
-});
-
-const AddTwoIntsResponse = ros.message('example_interfaces/srv/AddTwoInts', {
-  sum: ros.int64(),
+const AddTwoInts = ros.service('example_interfaces/srv/AddTwoInts', {
+  request: {
+    a: ros.int64(),
+    b: ros.int64(),
+  },
+  response: {
+    sum: ros.int64(),
+  },
 });
 
 const ws = new WebSocket(url);
@@ -18,12 +19,12 @@ const client = new RosCdrClient(ws);
 ws.onopen = async () => {
   const id = await client.createServiceClient(
     '/add_two_ints',
-    AddTwoIntsRequest.type,
+    AddTwoInts.type,
     {},
   );
-  const request = ros.serialize(AddTwoIntsRequest, { a: 2n, b: 3n });
+  const request = ros.serialize(AddTwoInts.request, { a: 2n, b: 3n });
   const response = await client.callService(id, request);
-  const deserialized = ros.deserialize(AddTwoIntsResponse, response);
+  const deserialized = ros.deserialize(AddTwoInts.response, response);
   console.log(`Result of add_two_ints: ${deserialized.sum}`);
   client.destroy(id);
   ws.close();
